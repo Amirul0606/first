014e147cf6e14d37411dd474b07fb3ccbd94cda5
Amirul <amirul.dev.mail@gmail.com>
2021-01-27 19:58:57 +0600

    add a new method

--- hello.java
+++ hello.java
@@ -7,3 +7,13 @@ public class HelloWorld {
         System.out.println("java code");
     }
 } 
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+ 
+        System.out.println("java code");
+    }
+} 
