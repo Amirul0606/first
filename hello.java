25c501b701720df8894efc87fc582ccb18a38b6b
Amirul <amirul.dev.mail@gmail.com>
2021-01-27 20:44:33 +0600

    add a new method in branch 2

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
  
-        System.out.println("java code");
+        System.out.println("branch b");
     }
 } 
 
@@ -14,6 +14,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
  
-        System.out.println("java code");
+        System.out.println("branch b");
     }
 } 
